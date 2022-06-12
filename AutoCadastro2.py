695a4dfa62158edb242445bad7d4b56f82f615fa
pedroh4659 <pedroh4659@gmail.com>
2022-06-12 16:32:30 -0300

    add requirements

--- AutoCadastro2.py
+++ AutoCadastro2.py
@@ -24,7 +24,7 @@ sleep(10)
 #clica na ultima conversa
 #navegador.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div/div[1]/div/div[4]/div[1]/div/div/div[1]').click()
 
-'''#abrindo maryhelper e mudando pra aba
+#abrindo maryhelper e mudando pra aba
 navegador.execute_script('window.open("about:blank", "secondtab");')
 navegador.switch_to.window('secondtab')
 navegador.get("https://app.maryhelp.net/clientes/cadastro")
@@ -33,7 +33,7 @@ navegador.get("https://app.maryhelp.net/clientes/cadastro")
 navegador.find_element_by_xpath('//*[@id="txtUser"]').send_keys("JOZEFALEIRO")
 navegador.find_element_by_xpath('//*[@id="txtPassword"]').send_keys("123456")
 navegador.find_element_by_xpath('//*[@id="login-form"]/button').click()
-'''
+
 
 
 wait
