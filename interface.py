07b65936bb8db0c8059c55df5a9930d712cfe847
Luiz Gustavo <52792623+LuizGustavoMM@users.noreply.github.com>
2022-07-11 19:55:06 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,18 +1,25 @@
+from optparse import Values
 import PySimpleGUI as sg
 
-dados = str('dados_cadastrais_dos_clientes_provenientes_do_webhook')
-
+def escolha():
 #layout
-layout=[[sg.Text('Lista mais recente de clientes: ',size=(30, 1), font='Lucida',justification='left')],
-        [sg.Listbox(values=[dados], select_mode='extended', size=(30, 6))],        
-        [sg.Button('Cadastrar', font=('Times New Roman',12)),sg.Button('Cancelar', key=('cadatrar'), font=('Times New Roman',12))]]
+    layout=[[sg.Text('Selecione se o cliente é PJ ou PF: ',size=(30, 1), font='Lucida',justification='left')], 
+    [sg.Button('Pessoa física', key=('pf'), font=('Times New Roman',12))],
+    [sg.Button('Pessoa juridica', key=('pj'), font=('Times New Roman',12))]] 
 #Window
-win = sg.Window('MaryHelper Autocadastro',layout)
+    win = sg.Window('MaryHelper Autocadastro', layout)
 #ler os valores
-e,v = win.read()
-#fechar janela após escolha
-win.close()
+    event, values = win.read()
+    if event in ('pf'):
+        print('pf')
+    else:
+        print('pj')
+                
+#fechar janela após escolha 
+    win.close()
 #acessa o valor escolhido na lista e adciona a uma string
-        
+
 #display string in a popup         
-sg.popup('O cliente será cadastrado nos próximos minutos!')
+    sg.popup('O cliente será cadastrado nos próximos minutos!')
+
+escolha()
